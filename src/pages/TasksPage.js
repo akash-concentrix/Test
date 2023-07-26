bea21e068ab6a851c26ca44f9638b0024200f141
Naveen Work <naveenwork@192.168.1.4>
2023-07-26 11:23:34 +0530

    initial commit

---
+++ src/pages/TasksPage.js
@@ -0,0 +1,307 @@
+import { Helmet } from 'react-helmet-async';
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
+import { useState, useEffect } from 'react';
+import {
+    Card,
+    Table,
+    Stack,
+    Paper,
+    Avatar,
+    Button,
+    Popover,
+    Checkbox,
+    TableRow,
+    MenuItem,
+    TableBody,
+    TableCell,
+    Container,
+    Typography,
+    IconButton,
+    TableContainer,
+    TablePagination,
+} from '@mui/material';
+import Label from '../components/label';
+import Iconify from '../components/iconify';
+import Scrollbar from '../components/scrollbar';
+import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
+import { TASK_LIST } from "../utils/apis";
+
+const TABLE_HEAD = [
+    { id: 'campaign_name', label: 'Campaign Name', alignRight: false },
+    // { id: 'promotion_link', label: 'Promotion Link', alignRight: false },
+    // { id: 'description', label: 'Description', alignRight: false },
+    { id: 'channel', label: 'Channel', alignRight: false },
+    { id: 'deadline', label: 'Deadline', alignRight: false },
+    { id: 'promoter_name', label: 'Promoter Name', alignRight: false },
+    { id: 'reward', label: 'Reward', alignRight: false },
+    { id: 'status', label: 'Campaign Status', alignRight: false },
+    { id: '' },
+];
+
+const STATUS_MAPPING = {
+    "TD": "Todo",
+    "IP": "Inprogress",
+    "CM": "Completed",
+}
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    if (query) {
+        return filter(array, (_tasks) => _tasks.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    }
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const TaskListPage = (props) => {
+
+    const [taskList, setTaskList] = useState([])
+    const [open, setOpen] = useState(null);
+    const [page, setPage] = useState(0);
+    const [order, setOrder] = useState('asc');
+    const [selected, setSelected] = useState([]);
+    const [orderBy, setOrderBy] = useState('name');
+    const [filterName, setFilterName] = useState('');
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    useEffect(() => {
+        const fetchTaskList = async () => {
+            const response = await fetch(TASK_LIST);
+            const data = await response.json();
+            return data;
+        };
+
+        const fetchData = async () => {
+            const data = await fetchTaskList();
+            setTaskList(data);
+        };
+
+        fetchData();
+    }, []);
+
+
+    const handleOpenMenu = (event) => {
+        setOpen(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setOpen(null);
+    };
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = taskList.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (_, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+        }
+        setSelected(newSelected);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setPage(0);
+        setRowsPerPage(parseInt(event.target.value, 10));
+    };
+
+    const handleFilterByName = (event) => {
+        setPage(0);
+        setFilterName(event.target.value);
+    };
+
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - taskList.length) : 0;
+
+    const filteredTasks = applySortFilter(taskList, getComparator(order, orderBy), filterName);
+
+    const isNotFound = !filteredTasks.length && !!filterName;
+
+    return (
+        <>
+            <Helmet>
+                <title> Tasks List </title>
+            </Helmet>
+
+            <Container>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                    <Typography variant="h4" gutterBottom>
+                        Tasks
+                    </Typography>
+                    <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+                        New Task
+                    </Button>
+                </Stack>
+
+                <Card>
+                    <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
+
+                    <Scrollbar>
+                        <TableContainer sx={{ minWidth: 800 }}>
+                            <Table>
+                                <UserListHead
+                                    order={order}
+                                    orderBy={orderBy}
+                                    headLabel={TABLE_HEAD}
+                                    rowCount={taskList.length}
+                                    numSelected={selected.length}
+                                    onRequestSort={handleRequestSort}
+                                    onSelectAllClick={handleSelectAllClick}
+                                />
+                                <TableBody>
+                                    {filteredTasks.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((campaignItem) => {
+                                        const selectedTask = selected.indexOf(campaignItem?.campaign?.id) !== -1;
+
+                                        return (
+                                            <TableRow hover key={campaignItem.id} tabIndex={-1} role="checkbox" selected={selectedTask}>
+                                                <TableCell padding="checkbox">
+                                                    <Checkbox checked={selectedTask} onChange={(event) => handleClick(event, campaignItem?.campaign?.id)} />
+                                                </TableCell>
+
+                                                <TableCell component="th" scope="row" padding="none">
+                                                    <Stack direction="row" alignItems="center" spacing={2}>
+                                                        <Avatar alt={campaignItem?.campaign?.name} src={campaignItem.avatarUrl} />
+                                                        <Typography variant="subtitle2" noWrap>
+                                                            {campaignItem?.campaign?.name}
+                                                        </Typography>
+                                                    </Stack>
+                                                </TableCell>
+
+                                                <TableCell align="left">{campaignItem?.campaign?.order?.channel}</TableCell>
+                                                <TableCell align="left">{new Date(campaignItem?.campaign?.order?.deadline).toLocaleDateString()}</TableCell>
+                                                <TableCell align="left">{campaignItem?.promoter?.first_name}</TableCell>
+                                                <TableCell align="left">{campaignItem?.reward}</TableCell>
+                                                <TableCell align="left">{STATUS_MAPPING[campaignItem?.campaign?.status]}</TableCell>
+                                                
+
+                                                <TableCell align="left">{campaignItem.is_active ? 'Yes' : 'No'}</TableCell>
+
+                                                <TableCell align="right">
+                                                    <IconButton size="large" color="inherit" onClick={handleOpenMenu}>
+                                                        <Iconify icon={'eva:more-vertical-fill'} />
+                                                    </IconButton>
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                    {emptyRows > 0 && (
+                                        <TableRow style={{ height: 53 * emptyRows }}>
+                                            <TableCell colSpan={6} />
+                                        </TableRow>
+                                    )}
+                                </TableBody>
+
+                                {isNotFound && (
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                                                <Paper
+                                                    sx={{
+                                                        textAlign: 'center',
+                                                    }}
+                                                >
+                                                    <Typography variant="h6" paragraph>
+                                                        Not found
+                                                    </Typography>
+
+                                                    <Typography variant="body2">
+                                                        No results found for &nbsp;
+                                                        <strong>&quot;{filterName}&quot;</strong>.
+                                                        <br /> Try checking for typos or using complete words.
+                                                    </Typography>
+                                                </Paper>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                )}
+                            </Table>
+                        </TableContainer>
+                    </Scrollbar>
+
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={taskList.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Card>
+            </Container>
+
+            <Popover
+                open={Boolean(open)}
+                anchorEl={open}
+                onClose={handleCloseMenu}
+                anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                PaperProps={{
+                    sx: {
+                        p: 1,
+                        width: 140,
+                        '& .MuiMenuItem-root': {
+                            px: 1,
+                            typography: 'body2',
+                            borderRadius: 0.75,
+                        },
+                    },
+                }}
+            >
+                <MenuItem>
+                    <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
+                    Edit
+                </MenuItem>
+
+                <MenuItem sx={{ color: 'error.main' }}>
+                    <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
+                    Delete
+                </MenuItem>
+            </Popover>
+        </>
+    );
+}
+
+export default TaskListPage;
---
+++ src/utils/apis.js
@@ -0,0 +1,2 @@
+
+export const TASK_LIST = `http://localhost:8000/smm/api/v1/campaign-items/`
