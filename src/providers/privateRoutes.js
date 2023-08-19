5c9b73dce4d906c73e6178cea44e20bd512b0fc3
Naveen <naveen.singh@outlook.in>
2023-08-19 17:07:49 +0530

    minor changes

--- src/providers/privateRoutes.js
+++ src/providers/privateRoutes.js
@@ -3,16 +3,14 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useSessionData } from '../hooks/useSessiondata';
 
 const PrivateRoutes = () => {
-    const { data, isLoading, isError, error } = useSessionData()
+    const { isLoading, isError, error } = useSessionData()
     if (isError || error) {
-        return <Navigate to="/" />
+        return <Navigate to="/login" />
     }
 
     if (isLoading) {
         return <div>Loading...</div>
     }
-
-    console.log(data, "data----")
 
     return <Outlet />
 }
--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,22 @@ import { Navigate, Routes, Route } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 
-import BlogPage from './pages/BlogPage';
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import Page404 from './pages/Page404';
-import ProductsPage from './pages/ProductsPage';
-import DashboardAppPage from './pages/DashboardAppPage';
-import TaskListPage from './pages/TasksPage';
+import Landing from "./pages";
+import BlogPage from './pages/blog';
+import UserPage from './pages/user';
+import LoginPage from './pages/login';
+import Page404 from './pages/404';
+import ProductsPage from './pages/products';
+import DashboardAppPage from './pages/dashboard';
+import TaskListPage from './pages/tasks';
 import PrivateRoutes from './providers/privateRoutes';
 
 const Router = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<LoginPage />} index={true} />
+      <Route path='/' element={<Landing />} index={true} />
+      <Route path='/login' element={<LoginPage />} index={true} />
       <Route element={<PrivateRoutes />}>
         <Route path="/dashboard" element={<DashboardLayout />} >
           <Route element={<Navigate to="/dashboard/app" />} index={true} />
